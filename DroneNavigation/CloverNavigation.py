60487ac09c2b48d0b59eb17a724cd13f523918e6
Eden Hallett <eden.n.hallett@student.uts.edu.au>
2024-10-10 11:40:35 +1100

    Added some clover navigation stuff

---
+++ DroneNavigation/CloverNavigation.py
@@ -0,0 +1,26 @@
+import rospy
+from clover import srv
+from std_srvs.srv import Trigger
+
+rospy.init_node('flight')
+
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
+set_position = rospy.ServiceProxy('set_position', srv.SetPosition)
+set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
+set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
+set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
+land = rospy.ServiceProxy('land', Trigger)
+
+# Takeoff and hover 1 m above the ground
+
+while True:
+    # for the number of lines in the text file:
+    # navigate(x=xDisplacement, y=yDisplacement, z=zDisplacement, frame_id='body', auto_arm=True)
+    # rospy.sleep(3) if you want it to pause (3s)
+
+
+
+# Perform landing
+land()
